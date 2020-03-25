319c2ad4285c35058826e4654a77e013ae91d6ce
Noe Romero <noerromero@gmail.com>
2020-03-25 16:55:24 -0500

    Version inicial

---
+++ ConsoleApp1/Oferta.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ConsoleApp1
+{
+
+    public class ListaLinea : List<Linea> { 
+
+    }
+
+    public class Linea {
+        public string Categoria { get; set; }
+        public string CuentaOferta { get; set; }
+        public int? CompuestaVariable { get; set; }
+        public string CUC { get; set; }
+        public string SAP { get; set; }
+        public int? FactorCuadre { get; set; }
+        public int? FactorRepeticion { get; set; }
+        public string Palanca { get; set; }
+        public string Tactica { get; set; }
+        public string TipoOferta { get; set; }
+
+        public int? IndicadorGratis { get; set; }
+    }
+
+    public class ListaOferta : List<Oferta>
+    {
+    }
+    public class Oferta
+    {
+
+        private string _formato;
+        
+        
+
+        public string NumeroOferta { get; set; }
+        public string Palanca { get; set; }
+        
+        public int? CompuestaVariable { get; set; }
+
+        public string Tactica { get; set; }
+        public IEnumerable<Producto> Productos { get; set; }
+        public string Formato { 
+            get {
+                if (Tactica == "Sets")
+                    _formato = "B";
+                else if (Tactica == "Individual")
+                    _formato = "I";
+                else _formato = "V";
+                return _formato;
+            }
+        } 
+        
+
+    }
+
+
+
+
+    public class OfertaComparer : IEqualityComparer<Oferta>
+    {
+
+        public bool Equals(Oferta x, Oferta y)
+        {
+            if (x.NumeroOferta == y.NumeroOferta && 
+                x.Palanca == y.Palanca
+                )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public int GetHashCode(Oferta obj)
+        {
+            return obj.NumeroOferta.GetHashCode();
+        }
+    }
+
+
+    public class ListaProducto: List<Producto> { 
+    }
+
+    public class Producto {
+
+        private int? _compuestaVariable;
+        private int? _unidades;
+
+        public Producto(int? compuestaVariable) {
+            _compuestaVariable = compuestaVariable;
+        }
+
+        public string CUC { get; set; }
+
+        public string Categoria { get; set; }
+
+        public string TO { get; set; }
+
+        public int? FactorCuadre { get; set; }
+
+        public int? FactorRepeticion { get; set; }
+
+        public IEnumerable<Tono> Tonos { get; set; }
+
+        public int? Unidades {
+            get {
+                if (_compuestaVariable == 0)
+                     _unidades = FactorRepeticion;
+                else
+                     _unidades = FactorCuadre * FactorRepeticion;
+                return _unidades;
+            }
+        }
+        
+    }
+
+    public class ProductoComparer : IEqualityComparer<Producto>
+    {
+        public bool Equals(Producto x, Producto y)
+        {
+            if (x.CUC == y.CUC &&
+                x.FactorCuadre == y.FactorCuadre &&
+                x.FactorRepeticion == y.FactorRepeticion
+                )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public int GetHashCode(Producto obj)
+        {
+            return obj.CUC.GetHashCode();
+        }
+    }
+
+    public class ListaTono : List<Tono> { 
+    }
+
+    public class Tono {
+        public string SAP { get; set; }
+
+        public string CUV { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertirCsvJson();    
+        }
+
+        private static void ConvertirCsvJson() {
+
+            ListaLinea lineas = new ListaLinea();
+
+            //LeerCsv
+            string csvFile = @"C:\dataPrueba\CRC05.csv";
+            string[] lines = File.ReadAllLines(csvFile);
+
+            foreach (var value in lines)
+            {
+                string[] columns = value.Split(',');
+                lineas.Add(new Linea {
+                    Categoria = columns[0],
+                    CuentaOferta = columns[1],
+                    CompuestaVariable = Convert.ToInt32(columns[2]),
+                    CUC = columns[3],
+                    SAP = columns[4],
+                    FactorCuadre = Convert.ToInt32(columns[5]),
+                    FactorRepeticion = Convert.ToInt32(columns[6]),
+                    Palanca = columns[7],
+                    Tactica = columns[8],
+                    TipoOferta = columns[9],
+                    IndicadorGratis = Convert.ToInt32(columns[10])
+                });
+            }
+
+            //Obtener ofertas
+            //lineas.Distinct()
+            //IEnumerable<Oferta>
+            IEnumerable<Oferta> ofertas= lineas.Select(x => new Oferta
+            {
+                NumeroOferta = x.CuentaOferta,
+                Palanca = x.Palanca,
+                CompuestaVariable = x.CompuestaVariable,
+                Tactica = x.Tactica
+            }).ToList().Distinct(new OfertaComparer());
+
+            int NumeroOfertas = ofertas.Count();
+
+
+            //Obtener productos por oferta
+
+            foreach (var ofer in ofertas)
+            {
+                ofer.Productos = lineas.Where(y => y.CuentaOferta==ofer.NumeroOferta)
+                    .Select(x => new Producto(ofer.CompuestaVariable)
+                    {
+                        CUC = x.CUC,
+                        Categoria = x.Categoria,
+                        TO = x.TipoOferta,
+                        FactorCuadre = x.FactorCuadre,
+                        FactorRepeticion = x.FactorRepeticion
+                    }).ToList().Distinct(new ProductoComparer());
+            }
+
+            NumeroOfertas = ofertas.Count();
+
+            //Obtener tonos por producto por oferta
+            foreach (var ofer in ofertas) {
+                foreach (var product in ofer.Productos) {
+                    product.Tonos = lineas.Where(y=>y.CuentaOferta==ofer.NumeroOferta && y.CUC==product.CUC)
+                        .Select(x => new Tono
+                        {
+                            SAP = x.SAP
+                        });
+                }
+            }
+
+            NumeroOfertas = ofertas.Count();
+
+            string ofertasJson = JsonConvert.SerializeObject(ofertas);
+
+            //Escribir archivo
+            string docPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "Ofertas.json")))
+            {
+                outputFile.WriteLine(ofertasJson);
+            }
+
+
+        }
+
+
+    }
+}
